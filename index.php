7568dc8565895c4b04c053de73a1e7a4b3e4ca4c
Utsab <utsab.k.saha@gmail.com>
2018-02-01 01:26:22 +0000

    Add all lab 1 files

---
+++ index.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html>
+<!--
+
+First Website
+and comment
+in html
+(comments can span multiple lines)
+
+-->
+
+<!-- This is the head -->
+<!-- All styles and javascript go inside the head -->
+    <head>
+        <meta charset="utf-8"/>
+        <title>Utsab Saha: Personal Website</title>
+        
+        <link href="https://fonts.googleapis.com/css?family=Rammetto+One" rel="stylesheet">
+        <link href="css/styles.css" rel="stylesheet" type="text/css" />
+    </head>
+<!-- closing head -->
+
+    <!-- This is the body -->
+    <!-- This is where we place the content of our website -->
+    <body>
+        
+        
+        <header>
+            <h1>Utsab Saha</h1>
+        </header>
+        <nav>
+            <hr width="50%"/>
+            
+            <a href="index.html">Home</a>
+            <a href="about.html">About</a>
+            <a href="contact.html">Contact</a>
+        </nav>
+        <!-- This is the footer -->
+        <!-- The footer goes inside the body but not always -->
+        
+        
+        <main>
+            <figure id="me">
+                <img src="img/juan_doe.png"/>
+            </figure>
+            <div id="welcomeText">
+                Hello! <br/>
+                <p>
+                    Thank you for visiting my professional portfolio website.
+                </p>
+                <p>
+                    I am a Software Engineer and have been recently hired at the Acme corporation.
+                </p>
+                <p>
+                    Feel free to contact me
+                </p>
+                
+                <br/><br/>
+                
+                <em>"With ordinary talent and extraordinary <strong>perseverance</strong>, all things are attainable"</em>
+                - <a href="http://abolition.e2bn.org/people_66.html">Thomas F. Buxton</a>
+            </div>
+        </main>
+        
+        <footer>
+            <hr>
+            Course name. 2017
+            
+        </footer>
+        <!-- closing footer -->
+        
+    </body>
+    <!-- closing body -->
+
+</html>
